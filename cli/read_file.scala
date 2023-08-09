9718c1de87511388012ec7f9cec6fd5dbfec970d
kazunoriboy <matunorigmnw@gmail.com>
2023-08-10 01:40:20 +0900

    calc max width of lines

--- cli/read_file.scala
+++ cli/read_file.scala
@@ -6,6 +6,11 @@ object reader {
   def main (args: Array[String]): Unit = {
     if (args.length > 0) {
       val lines = Source.fromFile(args(0)).getLines.toList
+
+      var maxWidth = 0
+      for (line <- lines)
+        maxWidth = maxWidth.max(widthOfLength(line))
+      
     }
     else
       Console.err.println("Please enter filename")
