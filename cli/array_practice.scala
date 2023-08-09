6426be1280ba14cf546e9b5c50aa42a991a99148
kazunoriboy <matunorigmnw@gmail.com>
2023-08-09 19:49:42 +0900

    fix typo

--- cli/array_practice.scala
+++ cli/array_practice.scala
@@ -1,6 +1,11 @@
 object array_practice {
-  @main def test() = {
-    val big = new java.math.BigInteger("12345")
-    print(big)
+  def test() = {
+    val greetStrings = new Array[String](3)
+    greetStrings(0) = "Hello"
+    greetStrings(1) = ", "
+    greetStrings(2) = "world!\n"
+
+    for (i <- 0 to 2)
+      print(greetStrings(i))
   }
 }
