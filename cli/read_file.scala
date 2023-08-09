ce0eed00d0b2088fb0b5ff3a618ffd106a47f148
kazunoriboy <matunorigmnw@gmail.com>
2023-08-10 01:39:51 +0900

    get all lines

--- cli/read_file.scala
+++ cli/read_file.scala
@@ -5,8 +5,7 @@ object reader {
 
   def main (args: Array[String]): Unit = {
     if (args.length > 0) {
-      for (line <- Source.fromFile(args(0)).getLines())
-        println(s"${line.length} $line")
+      val lines = Source.fromFile(args(0)).getLines.toList
     }
     else
       Console.err.println("Please enter filename")
