d0370b5f16af91a5e1ecff89625824d7b462182e
kazunoriboy <matunorigmnw@gmail.com>
2023-08-10 01:39:32 +0900

    define function of count length

--- cli/read_file.scala
+++ cli/read_file.scala
@@ -1,6 +1,8 @@
 import scala.io.Source
 
 object reader {
+  def widthOfLength(s: String) = s.length.toString.length
+
   def main (args: Array[String]): Unit = {
     if (args.length > 0) {
       for (line <- Source.fromFile(args(0)).getLines())
