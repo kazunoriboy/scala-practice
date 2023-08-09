7e27731a82dc61d5769383911821c22eb66f768c
kazunoriboy <matunorigmnw@gmail.com>
2023-08-09 20:16:00 +0900

    out put value of List object

--- cli/list.scala
+++ cli/list.scala
@@ -1,5 +1,12 @@
 object list_prac {
-  @main def sample() = {
-    val oneTwoThree = List(1, 2, 3)
+  def sample() = {
+    val oneTwo = List(1, 2)
+    val threeFour = List(3, 4)
+    val oneTwoThreeFour = oneTwo ::: threeFour
+    println(oneTwo)
+    println(threeFour)
+    println(oneTwoThreeFour)
+    
+  }
   }
 }
