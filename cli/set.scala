ed9c64503593fc05f948cc4687c7a1d2cb811027
kazunoriboy <matunorigmnw@gmail.com>
2023-08-10 00:44:50 +0900

    use mutable set
    set is collection of unique elements.

--- cli/set.scala
+++ cli/set.scala
@@ -1,8 +1,16 @@
+import scala.collection.mutable
+
 object set_prac {
-  @main def main() = {
+  def immutableSet() = {
     var jetSet = Set("Boeing", "Airbus")
     jetSet += "Lear"
     println(jetSet.contains("Cessna"))
     println(jetSet)
+  }
+
+  @main def mutableSet() = {
+    val movieSet = mutable.Set("Hitch", "Poltergeist")
+    movieSet += "Shrek"
+    println(movieSet)
   }
 }
