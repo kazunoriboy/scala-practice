bead106eaa1daff4616edf2ed84e25c0bd7293c5
kazunoriboy <matunorigmnw@gmail.com>
2023-08-10 09:58:55 +0900

    In Scala return type can be omitted, but normaly, it is usually stated because people need to infer by oneself

--- cli/chapter04/checkSumAccumulator.scala
+++ cli/chapter04/checkSumAccumulator.scala
@@ -1,5 +1,5 @@
 class ChecksumAccumulator {
   private var sum = 0
-  def add(b: Byte) = sum += b
-  def checksum() = ~(sum & 0xFF) + 1
+  def add(b: Byte): Unit = sum += b
+  def checksum(): Int = ~(sum & 0xFF) + 1
 }
