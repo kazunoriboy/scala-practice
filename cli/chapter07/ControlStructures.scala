5beefe6a0ed69cdf282afdf1f7834f375805af13
kazunoriboy <matunorigmnw@gmail.com>
2023-08-12 17:56:05 +0900

    use while syntax

--- cli/chapter07/ControlStructures.scala
+++ cli/chapter07/ControlStructures.scala
@@ -1,6 +1,7 @@
 object ControlStructures {
   def main(args: Array[String]) = {
     println(ifSyntax(args))
+    println(whileSyntax(10L, 32L))
   }
 
   def ifSyntax(args: Array[String]) = {
@@ -8,5 +9,15 @@ object ControlStructures {
       if (!args.isEmpty) args(0)
       else "default.txt"
   }
+
+  def whileSyntax(x: Long, y: Long) = {
+    var a = x
+    var b = y
+    while (a != 0) {
+      val temp = a
+      a = b % a
+      b = temp
+    }
+    b
   }
 }
