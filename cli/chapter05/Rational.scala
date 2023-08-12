892e90bf195cc4b2488af40fffe81d5897f4d4ca
kazunoriboy <matunorigmnw@gmail.com>
2023-08-12 16:10:52 +0900

    this code has error because add can only access to receiver.

--- cli/chapter05/Rational.scala
+++ cli/chapter05/Rational.scala
@@ -1,12 +1,17 @@
 class Rational(n: Int, d: Int) {
   require(d != 0)
   override def toString = s"$n/$d"
+
+  def add(that: Rational): Rational = {
+    new Rational(n * that.d + that.n * d, d * that.d)
+  }
 }
 
 object Rational {
   @main def main(): Unit = {
     val r = new Rational(1, 2)
-    println(r)
+    val r2 = new Rational(2, 3)
+    println(r.add(r2))
   }
 }
 
