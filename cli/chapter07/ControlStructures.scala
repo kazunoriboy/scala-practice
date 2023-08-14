95bde872b5ce6dcd6a4db407de14260ed729096a
kazunoriboy <matunorigmnw@gmail.com>
2023-08-14 17:44:07 +0900

    add multiple condition in for

--- cli/chapter07/ControlStructures.scala
+++ cli/chapter07/ControlStructures.scala
@@ -28,7 +28,11 @@ object ControlStructures {
   
   def filteringFor() = {
     val filesHere = (new java.io.File(".")).listFiles
-    for (file <- filesHere if file.getName.endsWith(".scala"))
+    for (
+       file <- filesHere
+       if file.isFile
+       if file.getName.endsWith(".scala")
+    )
       println(file)
   }
 }
