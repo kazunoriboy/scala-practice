0ed3f242339bf9602196c59341eabef00ee1285c
kazunoriboy <matunorigmnw@gmail.com>
2023-08-22 12:15:36 +0900

    define methods in singleton object

---
+++ cli/chapter08/LongLines.scala
@@ -0,0 +1,22 @@
+import scala.io.Source
+
+object FindLine {
+  def main(args: Array[String]) = {
+    val width = args(0).toInt
+    for (arg <- args.drop(1))
+      LongLine.processFile(arg, width)
+  }
+}
+
+object LongLine {
+  def processFile(filename: String, width: Int) = {
+    val source = Source.fromFile(filename)
+    for (line <- source.getLines())
+      processLine(filename, width, line)
+  }
+
+  private def processLine(filename: String, width: Int, line: String) = {
+    if (line.length > width)
+      println(filename + ": " + line.trim)
+  } 
+}
