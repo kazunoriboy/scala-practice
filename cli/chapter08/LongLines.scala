d12fcd9d890f5c6f06c85ed46ce86c87123fddba
kazunoriboy <matunorigmnw@gmail.com>
2023-08-22 12:20:56 +0900

    ref: defined a private function by use local function

--- cli/chapter08/LongLines.scala
+++ cli/chapter08/LongLines.scala
@@ -10,13 +10,12 @@ object FindLine {
 
 object LongLine {
   def processFile(filename: String, width: Int) = {
+    def processLine(filename: String, width: Int, line: String) = {
+      if (line.length > width)
+        println(filename + ": " + line.trim)
+    }
     val source = Source.fromFile(filename)
     for (line <- source.getLines())
       processLine(filename, width, line)
   }
-
-  private def processLine(filename: String, width: Int, line: String) = {
-    if (line.length > width)
-      println(filename + ": " + line.trim)
-  } 
 }
