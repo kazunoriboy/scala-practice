1d41fd69a79fe8fa7c7fe6b2373d0169484c219e
kazunoriboy <matunorigmnw@gmail.com>
2023-09-08 09:52:56 +0900

    refactor: define fileMatching method and use it in other methods.
    3 other methods pass a function value to fileMatching

--- cli/chapter09/constructive.scala
+++ cli/chapter09/constructive.scala
@@ -2,15 +2,16 @@ object FileMatcher {
   private def filesHere = (new java.io.File(".")).listFiles
 
   def fileEnding(query: String) =
-    for (file <- filesHere; if file.getName.endsWith(query))
-      yield file
+    fileMatching(query, _.endsWith(_))
 
   def filesContaining(query: String) =
-    for (file <- filesHere; if file.getName.contains(query))
-      yeild file
+    fileMatching(query, _.contains(_))
 
   def fileRegex(query: String) =
-    for (file <- filesHere; if file.getName.matches(query))
-      yield file
+    fileMatching(query, _.matches(_))
 
+  def filesMatching(query: String, matcher: (String, String) => Boolean) = {
+    for (file <- filesHere; if matcher(file.getName, query))
+      yield file:
+  }
 }
