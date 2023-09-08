ef9875a7514a33f86a12663fcc328d56c9a9f842
kazunoriboy <matunorigmnw@gmail.com>
2023-09-08 09:50:24 +0900

    define method of search file by regular expression

--- cli/chapter09/constructive.scala
+++ cli/chapter09/constructive.scala
@@ -8,4 +8,9 @@ object FileMatcher {
   def filesContaining(query: String) =
     for (file <- filesHere; if file.getName.contains(query))
       yeild file
+
+  def fileRegex(query: String) =
+    for (file <- filesHere; if file.getName.matches(query))
+      yield file
+
 }
