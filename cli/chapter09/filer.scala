9b47789a0b0f07c8513ea1eb8e8555599e51d14c
kazunoriboy <matunorigmnw@gmail.com>
2023-09-30 00:22:40 +0900

    open and close file object

---
+++ cli/chapter09/filer.scala
@@ -0,0 +1,10 @@
+object filer {
+  def withPrintWriter(file: File, op: PrintWriter => Unit) = {
+    val writer = new PrintWriter(file)
+    try {
+      op(writer)
+    } finally {
+      writer.close()
+    }
+  }
+}
