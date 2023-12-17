4ad07775204c6b4e7db643297080546550dc5ffa
kazunoriboy <matunorigmnw@gmail.com>
2023-12-17 18:35:48 +0900

    move sub classes into object to be private

--- cli/chapter10/element.scala
+++ cli/chapter10/element.scala
@@ -21,27 +21,28 @@ abstract class Element {
   override def toString = contents mkString "\n"
 }
 
-class ArrayElement(
-  val contents: Array[String]
-) extends Element {
-}
-
-class LineElement(s: String) extends Element {
-  val contents = Array(s)
-  override def width = s.length
-  override def height = 1
-}
-
-class UniformElement(
-  ch: Char,
-  override val width: Int,
-  override val height: Int
-) extends Element {
-  private val line = ch.toString * width
-  def contents = Array.fill(height)(line)
-}
 
 object Element {
+  private class ArrayElement(
+    val contents: Array[String]
+  ) extends Element {
+  }
+
+  private class LineElement(s: String) extends Element {
+    val contents = Array(s)
+    override def width = s.length
+    override def height = 1
+  }
+
+  private class UniformElement(
+    ch: Char,
+    override val width: Int,
+    override val height: Int
+  ) extends Element {
+    private val line = ch.toString * width
+    def contents = Array.fill(height)(line)
+  }
+
   def elem(contents: Array[String]): Element =
     new ArrayElement(contents)
   
