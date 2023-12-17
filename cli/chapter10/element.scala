2f556115c75341c9b6faef8e9997278c724c39dc
kazunoriboy <matunorigmnw@gmail.com>
2023-12-17 18:32:37 +0900

    use factory

--- cli/chapter10/element.scala
+++ cli/chapter10/element.scala
@@ -1,16 +1,24 @@
+import Element.elem
+
 abstract class Element {
   def contents: Array[String]
-  def height: Int = contents.length
+
   def width: Int = if (height == 0) 0 else contents(0).length
+
+  def height: Int = contents.length
+
   def above(that: Element): Element =
-    new ArrayElement(this.contents ++ that.contents)
+    elem(this.contents ++ that.contents)
+
   def beside(that: Element): Element = {
-    new ArrayElement(
+    elem(
       for (
         (line1, line2) <- this.contents zip that.contents
       ) yield line1 + line2
     )
   }
+
+  override def toString = contents mkString "\n"
 }
 
 class ArrayElement(
