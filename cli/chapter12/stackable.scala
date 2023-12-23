14afef129f5b63ef45ea26aacf0d14cc72bd0cde
kazunoriboy <matunorigmnw@gmail.com>
2023-12-24 00:09:35 +0900

    defined int queue

--- cli/chapter12/stackable.scala
+++ cli/chapter12/stackable.scala
@@ -1,4 +1,21 @@
+import scala.collection.mutable.ArrayBuffer
+
 abstract class IntQueue {
   def get(): Int
   def put(x: Int): Unit
+}
+
+class BasicIntQueue extends IntQueue {
+  private val buf = new ArrayBuffer[Int]
+  def get() = buf.remove(0)
+  def put(x: Int) = { buf += x}
+}
+
+@main def testStackable(): Unit = {
+  val queue = new BasicIntQueue
+  queue.put(10)
+  queue.put(20)
+  println(queue.get())
+  println(queue.get())
+
 }
