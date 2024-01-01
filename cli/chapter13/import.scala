e1204fba4d58b3e4b27f81a9024be7f54ccde7a4
kazunoriboy <matunorigmnw@gmail.com>
2024-01-02 00:10:17 +0900

    import

--- cli/chapter13/import.scala
+++ cli/chapter13/import.scala
@@ -11,3 +11,12 @@ def showFruit(fruit: Fruit) = {
   import fruit._
   println(name + "s are " + color)
 }
+
+import java.util.regex
+
+class AStarB {
+  // java.util.regex.Patternを参照
+  val pat = regex.Pattern.compile("a*b")
+}
+
+
