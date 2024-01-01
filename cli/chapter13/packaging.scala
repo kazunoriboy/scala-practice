24ce76e134212b90cac6e6370380cc1549f9a55b
kazunoriboy <matunorigmnw@gmail.com>
2024-01-01 19:17:12 +0900

    package can nest
    so, we can define class and its test, but these are not same package.

--- cli/chapter13/packaging.scala
+++ cli/chapter13/packaging.scala
@@ -4,3 +4,15 @@ class Navigator
 package  bobsrocket.navigation {
   class Navigator
 }
+
+package bobsrockets {
+  package navigation {
+    class Navigator {
+      val map = new StarMap
+    }
+
+    package test {
+      class NavigatorSuite
+    }
+  }
+}
