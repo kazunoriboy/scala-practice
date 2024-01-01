93f3bae0d09796f8d8fd9f304777cb9062a1afdb
kazunoriboy <matunorigmnw@gmail.com>
2024-01-01 23:49:59 +0900

    can abbreviate brackets

--- cli/chapter13/packaging.scala
+++ cli/chapter13/packaging.scala
@@ -1,16 +1,16 @@
-package bobsrockets {
-  package navigation {
-    class Navigator {
-      val map = new StarMap
-    }
-    class StarMap
+package bobsrockets
+package navigation {
+  class Navigator {
+    val map = new StarMap
   }
-  class Ship {
-    val nav = new navigation.Navigator
-  }
-  package fleets {
-    class Fleet {
-      def addShip() { new Ship }
-    }
-  }
+  class StarMap
 }
+class Ship {
+  val nav = new navigation.Navigator
+}
+package fleets
+class Fleet {
+  def addShip() { new Ship }
+}
+
+
