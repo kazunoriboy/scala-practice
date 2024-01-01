8a5e640f52d20eca502faafdf3299dd18bb8b272
kazunoriboy <matunorigmnw@gmail.com>
2024-01-02 00:32:36 +0900

    protected member

--- cli/chapter13/access.scala
+++ cli/chapter13/access.scala
@@ -7,3 +7,16 @@ class Outer {
   }
   (new Inner).f() // error: f is not accessible
 }
+
+package p {
+  class Super {
+    protected def f() = { pirntln("f") }
+  }
+  class Sub extends Super {
+    f() // OK
+  }
+  class Other {
+    (new Super).f() // error: f is not accessible in Scala. Java would accept this
+  }
+}
+
